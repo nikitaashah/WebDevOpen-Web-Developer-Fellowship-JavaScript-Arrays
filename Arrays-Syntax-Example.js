0124e5f116476260341b672a52262fc57f454e12
Nikita Shah <nikita12.shah@gmail.com>
2024-11-28 15:10:07 +0530

    Adding an example for array syntax.

---
+++ Arrays-Syntax-Example.js
@@ -0,0 +1,11 @@
+let listOfPuppies = ["shadow", "mocha", "ranger"];
+let emptyArray = [];
+let puppyAges = [0, 1, 2];
+
+// We can mix strings and numbers!
+let puppyAgeDescriptions = ["two months", "thirteen months", 2];
+
+let puppiesAndAges = [
+    ["shadow", "mocha", "ranger"],
+    [0, 1, 2],
+];      // arrays inside arrays!
