47ab63f203995ab50155451e985005e09902eadc
Nikita Shah <nikita12.shah@gmail.com>
2024-11-28 16:55:41 +0530

    Example for changing arrays, access and assign.

---
+++ Changing-Arrays-Example.js
@@ -0,0 +1,6 @@
+let dogAges = [6, 9, 10];
+dogAges[0];     // 6
+dogAges[0] = 7;     // happy birthday to the first dog!
+dogAges[0];     // 7
+dogAges[3] = 5;     // a fourth dog, five years old!
+console.log(dogAges);   // [7, 9, 10, 5]
