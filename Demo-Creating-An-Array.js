21c9215fb0566c2f88e67b468656524caaa832d8
Nikita Shah <nikita12.shah@gmail.com>
2024-11-28 14:46:18 +0530

    Adding demo for creating an array.

---
+++ Demo-Creating-An-Array.js
@@ -0,0 +1,10 @@
+let myFavouriteFruits = ["banana", "cherry", "mango"];
+
+let myLuckyNumbers = [4, 8, 15, 16, 23, 42];
+
+// Add the two arrays.
+let combinedArray = myFavouriteFruits + myLuckyNumbers;
+
+console.log(myFavouriteFruits);
+console.log(myLuckyNumbers);
+console.log(combinedArray);
