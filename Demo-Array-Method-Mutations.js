a2d69952c5524ec6dc35091deb424381e6b51916
Nikita Shah <nikita12.shah@gmail.com>
2024-11-30 22:10:24 +0530

    Adding demo for array method mutations in Javascript.

---
+++ Demo-Array-Method-Mutations.js
@@ -0,0 +1,11 @@
+let myFavoriteFruits = ["banana", "cherry", "mango"];
+console.log(myFavoriteFruits);
+
+// does push change the array?
+myFavoriteFruits.push("pear");
+console.log(myFavoriteFruits);
+
+// what about pop?
+let last = myFavoriteFruits.pop();
+console.log(myFavoriteFruits);
+console.log(last);
